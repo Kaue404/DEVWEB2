0c5bff89027434abe62604a4c7532ce370aaac71
Kaue404 <jkaue986@gmail.com>
2023-08-11 20:28:50 -0300

    11/08/2023

---
+++ exemplo1.ts
@@ -0,0 +1,2 @@
+let message: string = "Olá mundo, meu primeiro programa";
+console.log(message);
---
+++ exemplo2.ts
@@ -0,0 +1,18 @@
+namespace exemplo2 {
+    //VAR, LET, CONST
+    let numero: number;
+    numero = 5;
+
+    let resultado: number;
+    resultado = numero + numero;
+    console.log("A soma entre " + numero + " Com " + numero + " é igual a: " + resultado);
+
+    resultado = numero - 4;
+    console.log(`A subtração entre ${numero} com 4 é igual a: ${resultado}`);
+
+    resultado = numero * 3;
+    console.log(`A multiplicação entre ${numero} vezes 3 é igual a: ${resultado}`);
+
+    resultado = numero / 2;
+    console.log(`A divisão do ${numero} por 2 é igual a: ${resultado}`);
+}
