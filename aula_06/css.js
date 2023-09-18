272d10e0b4f44e6248d3c6f34d9a8653234e2a82
Kauê José <jkaue986@gmail.com>
2023-09-18 20:04:17 -0300

    (18/09)

---
+++ aula_06/css.js
@@ -0,0 +1,43 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            display: "0",
+            numeroAtual: null,
+            numeroAnterior: null,
+            operador: null
+        }
+    },
+    methods: {
+        lidarBotao(botao) {
+            //console.log(botao)
+            switch (botao) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    this.lidarOperador(botao);
+                    break;
+                case "=":
+                    this.lidarIgual(botao);
+                    break;
+                case ".":
+                    this.lidarDecimal(botao);
+                    break;
+                default:
+                    this.lidarNumeros(botao);
+                    break;
+
+            }
+
+            lidarNumeros(numero) {
+                if (this.display === 0) {
+
+                } else {
+                    this.display += numero.
+                        toString();
+                }
+            }
+        }
+    }
+}).mount("#app")
